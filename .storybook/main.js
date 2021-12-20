320bcc39f9996b6bcb683086adfdc375d1ca5d2d
Evan Morrison <evanmorrison@filevine.com>
2021-12-20 12:47:40 -0700

    update for tailwind 3.0, update to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
-  ],
-  "framework": "@storybook/svelte",
-  "svelteOptions": {
-    "preprocess": import("../svelte.config.js").preprocess
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	core: {
+		builder: 'storybook-builder-vite'
+	},
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
+	framework: '@storybook/svelte',
+	svelteOptions: {
+		preprocess: import('../svelte.config.js').preprocess
+	},
+	webpackFinal: async (config) => {
+		config.resolve = {
+			...config.resolve,
+			alias: {
+				...config.resolve.alias,
+				svelte: path.resolve(__dirname, '..', 'node_modules', 'svelte')
+			},
+			mainFields: ['svelte', 'browser', 'module', 'main']
+		};
+
+		config.module.rules.push({
+			resolve: {
+				fullySpecified: false,
+				extensions: ['.js', '.ts']
+			}
+		});
+
+		return config;
+	}
+};
